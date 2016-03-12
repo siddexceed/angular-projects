dbace2fb10f1be95e11dce77c7e678e0b6e87cb0
siddexceed <sidhm12@gmail.com>
2016-03-12 23:49:45 +0530

    Initial COmmit to my repo

---
+++ mean-first/mean-todo/src/app.js
@@ -0,0 +1,7 @@
+'use strict';
+var express = require('express');
+var app=express();
+app.use("/",express.static('public'));
+app.listen(3000,function(){
+    console.log('The server is running at port 3000!');
+});
